8febf8db1a3435de12fc1255f1849d68e29e17df
miikargh <mamoilanen@gmail.com>
2018-02-06 16:11:42 +0200

    Added sentiment gauge

--- pages/tweets.js
+++ pages/tweets.js
@@ -1,10 +1,12 @@
 import fetch from 'isomorphic-unfetch'
 import Layout  from '../components/Layout'
 import TweetEmotionPie from '../components/TweetEmotionPie'
+import Gauge from 'react-svg-gauge'
 
-const tweets = (props) => (
+const tweets = ({ tweets, sentimentScore }) => (
   <Layout>
-    <TweetEmotionPie title="Puppies" tweets={props.tweets} />
+    <TweetEmotionPie title="Puppies" tweets={ tweets } />
+    <Gauge label="Sentiment score" value={ sentimentScore } min={-1} max={1} width={400} height={320} />
   </Layout>
 )
 
@@ -17,8 +19,19 @@ tweets.getInitialProps = async () => {
   const resp = await fetch(`${scraperURL}/?keywords=${keywords}&user=${user}`)
   const posts = await resp.json()
 
+  const sentiments = posts
+    .map(p => p.sentiment)
+    .filter(p => p)
+  
+  const totalSentiment = sentiments
+    .reduce((acc, cur) => acc + cur)
+
+  const sentimentScore = (totalSentiment / sentiments.length).toFixed(2)
+
+
   return {
-    tweets: posts
+    tweets: posts,
+    sentimentScore: sentimentScore
   }
 
 }
