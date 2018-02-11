21c133f2f3f2ba8e139148f9be5a17388dd50dd0
miikargh <mamoilanen@gmail.com>
2018-02-11 09:53:25 +0200

    Removed old version

--- components/Login.js
+++
@@ -1,6 +0,0 @@
-export default ({ user, login, logout }) => (
-    <button
-        className="mdl-button mdl-js-button mdl-button--raised mdl-button--accent"
-        onClick={user ? logout : login}
-        >{user ? "Sing Out" : "Sign in"}</button>
-)
