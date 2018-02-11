e51b07edd67df2971ddb6b1674fbb52d58293bc5
miikargh <mamoilanen@gmail.com>
2018-02-11 13:14:26 +0200

    Fixed units bug

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -1,7 +1,7 @@
 export default ({ name, coors, height, width, color }) => (
   <div>
     <iframe
-      src={`//forecast.io/embed/#lat=${coors.lat}&lon=${coors.lng}&name=${name}&=units=si&color=${color}`}
+      src={`//forecast.io/embed/#lat=${coors.lat}&lon=${coors.lng}&name=${name}&units=si&color=${color ? color : ''}`}
       height={ height }
       width={ width }
       frameBorder="0"></iframe>
